853711e7e1838b3f340337db819f388aaa7bdfff
Diyaz Yakubov <diyaz.yakubov@gmail.com>
2018-02-05 16:49:49 +0300

    replace-proxy

---
+++ patterns/structural/proxy/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using proxy.models;
+
+namespace proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            INotebook notebook = new CacheProxy();
+            notebook.Write("dima","+79098542296");
+            notebook.Write("mike","+19062583654");
+            notebook.Write("damir","+23658541123");
+            notebook.Write("rob","+77777777777");
+            
+            notebook.Read("dima");
+            notebook.Read("dima");
+            notebook.Read("mike");
+            notebook.Read("mike");
+            notebook.Read("damir");
+            notebook.Read("rob");
+            notebook.Read("rob");
+        }
+    }
+}
