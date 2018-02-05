2ee93814ccb3f59ce88d9c0d8afb8ecb42beaa58
Diyaz Yakubov <diyaz.yakubov@gmail.com>
2018-02-05 18:08:54 +0300

    init-composite

---
+++ patterns/structural/composite/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
