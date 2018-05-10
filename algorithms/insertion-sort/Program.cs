b76fe4929c4e3a4a4f9869cb39ba38845fa12067
Diyaz Yakubov <diyaz.yakubov@gmail.com>
2018-05-10 15:14:41 +0300

    - implement insertion-sort

--- algorithms/insertion-sort/Program.cs
+++ algorithms/insertion-sort/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace insertion_sort
 {
@@ -6,7 +7,43 @@ namespace insertion_sort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = new int[]{2,3,4,5,1,23,4,6,8,3,7};
+            System.Console.WriteLine("init");
+            System.Console.WriteLine(string.Join(",", array));
+            array.InsertionSort();
+            System.Console.WriteLine("sort");
+            System.Console.WriteLine(string.Join(",", array));
+            System.Console.WriteLine("sort decs");
+            array.InsertionSortDecs();
+            System.Console.WriteLine(string.Join(",", array));
+         }
+    }
+
+    public static class ArrayExtension{
+        public static void InsertionSort(this int[] array){
+            for(int j=1; j<array.Length; j++){
+                var key = array[j];
+                int i = j-1;
+                while (i>=0 && array[i]>key)
+                {
+                    array[i+1]=array[i];
+                    i--;
+                }
+                array[i+1] = key;
+            }
+        }
+
+        public static void InsertionSortDecs(this int[] array){
+            for(int j=1; j<array.Length; j++){
+                var key = array[j];
+                int i = j-1;
+                while (i>=0 && array[i]<key)
+                {
+                    array[i+1]=array[i];
+                    i--;
+                }
+                array[i+1] = key;
+            }
         }
     }
 }
