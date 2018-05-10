f7e08de35469910e5b25feb6f6d4be3179544e61
Diyaz Yakubov <diyaz.yakubov@gmail.com>
2018-05-10 14:56:54 +0300

    - init

---
+++ algorithms/insertion-sort/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace insertion_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
