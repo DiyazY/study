b9980e25c70e2bbceda513f769d8118d5857671c
Diyaz Yakubov <diyaz.yakubov@gmail.com>
2018-05-14 17:25:42 +0300

    'selection-sort

--- algorithms/selection-sort/Program.cs
+++ algorithms/selection-sort/Program.cs
@@ -6,7 +6,31 @@ namespace selection_sort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = new int[]{2,3,4,5,1,23,4,6,8,3,7};
+            System.Console.WriteLine("init");
+            System.Console.WriteLine(string.Join(",", array));
+            array.SelectionSort();
+            System.Console.WriteLine("sort");
+            System.Console.WriteLine(string.Join(",", array));
         }
+    }
+
+    public static class ArraySort{
+        public static void SelectionSort(this int[] array){
+            for(int i=0; i<array.Length-1;i++){
+                var min = array[i];
+                var index = i;
+                for(int j = i+1; j<array.Length;j++){
+                    if(min>array[j]){
+                        min=array[j];
+                        index = j;
+                    }
+                }
+                if(index!=i){
+                    array[index]=array[i];
+                    array[i]=min;
+                } 
+            }
+        } 
     }
 }
