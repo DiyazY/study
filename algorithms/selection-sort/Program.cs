d96faea2d808f05760a0e7b57ad185cb7c0a05f2
Diyaz Yakubov <diyaz.yakubov@gmail.com>
2018-05-14 17:05:18 +0300

    'init

---
+++ algorithms/selection-sort/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace selection_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
