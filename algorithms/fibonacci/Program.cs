d41bedf42d5869f2cb2aeee0fc20cf41b63fc1d0
Diyaz Yakubov <diyaz.yakubov@appgear.ca>
2018-08-09 10:05:41 +0300

    fibonacci

---
+++ algorithms/fibonacci/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int result = 0;
+            int first = 0;
+            int second = 1;
+            for(int i=2;i<=n;i++){
+                result = first + second;
+                first = second;
+                second = result;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
